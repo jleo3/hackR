58070eded681ea3587f56e2a37c4b7875b32ec70
The Man <joe_leo46@yahoo.com>
2009-11-14 11:53:07 -0500

    - adding a Rakefile with support for tests - hackr should now create a root directory and two subdirectories

--- lib/hack_r.rb
+++ lib/hack_r.rb
@@ -1,7 +1,23 @@
 module HackR
 
+  def hack(args)
+    output_intro_message
+    create_root_dir_named args.first
+    create_subdirs
+  end
+
   def output_intro_message
     puts "Hacking together a Ruby work environment..."
   end
+
+  def create_root_dir_named dir_name
+    FileUtils.mkdir dir_name 
+  end
+
+  def create_subdirs
+    FileUtils.mkdir 'lib'
+    FileUtils.mkdir 'test'
+  end
+      
 
 end
--- test/hackr_test.rb
+++ test/hackr_test.rb
@@ -6,9 +6,32 @@ require 'mocha'
 class HackRTest < Test::Unit::TestCase
   include HackR
 
+  def setup
+    @root_hack_dir = "hacker_playground"
+  end
+
   def test_puts_intro_message
     self.expects(:puts).with("Hacking together a Ruby work environment...")
     output_intro_message
+  end
+
+  def test_create_root_dir
+    FileUtils.expects(:mkdir).with(@root_hack_dir)
+    create_root_dir_named @root_hack_dir
+  end
+
+  def test_create_subdirs
+    FileUtils.expects(:mkdir).with("lib")
+    FileUtils.expects(:mkdir).with("test")
+    create_subdirs
+  end
+
+  def test_hack_executes_commands
+    command_line_args = [@root_hack_dir]
+    self.expects :output_intro_message
+    self.expects(:create_root_dir_named).with(@root_hack_dir)
+    self.expects :create_subdirs
+    hack command_line_args
   end
 
 end
